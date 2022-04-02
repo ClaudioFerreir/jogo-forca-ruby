0f9fd50c38c7edb0d09c3a12e3a8e88f8d2c3c4c
claudioferreira <claudioferreirajr@hotmail.com>
2022-04-01 23:51:48 -0300

    logic ok

--- Jogos/Jogo_forca/app.rb
+++ Jogos/Jogo_forca/app.rb
@@ -1,20 +1,33 @@
 # user input a word
 puts 'Hi!! Write a word for your friend try to guess:'
-word = gets.chomp.to_s.split('')
-p word
+word = gets.chomp.strip.to_s.chars
 
-puts '_ ' * word.strip.length
 # another user input a letter that he hope was part of the word
 puts 'Try to guess a letter in this word:'
 letter = gets.chomp.to_s
+
 # if the letter match with one include on the word the letter is show in the word
-word_true = Array.new(word.length)
-if word.include? letter
-  puts 'Good work!!!'
-else
-  puts 'Try again :('
+word_guess = Array.new(word.length, '_')
+p word_guess
+while word.include? letter
+  i = word.index(letter)
+  word_guess[i] = letter
+  word[i] = '*'
 end
+p word
+p word_guess
+
 # else the letter dont match, put a part of body of human and show the wrong letter in a list
+wrong_letters = []
+unless word.include? letter
+  puts 'Wrong letter!!!'
+  wrong_letters << letter
+  p wrong_letters
+end
 
 # if you try a number of plays you lose
+
 # if you match all the letters you win
+unless word_guess.include? '_'
+  puts 'You win!!! Congratulations!!!'
+end
