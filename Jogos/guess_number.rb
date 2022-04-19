5873966af76b6144935264dd4a16f3bcff2b68ed
claudioferreira <claudioferreirajr@hotmail.com>
2022-04-18 23:56:56 -0300

    update

--- Jogos/guess_number.rb
+++ Jogos/guess_number.rb
@@ -9,17 +9,17 @@ class Guess_number
   def try_guess(number = 0)
     if number == @number
       @won = true
-      return 'You win!!!'
+      'You win!!!'
     elsif number > @number
-      return 'Your number is greater than ...'
+      'Your number is greater than ...'
     else
-      return 'Your number is smaller than ...'
+      'Your number is smaller than ...'
     end
   end
 
   game = Guess_number.new
 
-  until game.won do
+  until game.won
     puts '-------------------------------'
     puts 'Type a number (1-10):'
     number = gets.to_i
